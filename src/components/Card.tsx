20553b47182d0e917f3e05926bf4d96da6365a32
maiano <maiano@yandex.ru>
2025-07-11 21:03:45 +0200

    feat: add information to the Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,8 @@ type CardProps = {
 
 export class Card extends Component<CardProps> {
   render() {
-    const { name, status, species, gender, image } = this.props.character;
+    const { name, status, species, gender, image, origin, location } =
+      this.props.character;
 
     return (
       <div className="flex items-center gap-4 p-4 dark:bg-gray-400 bg-gray-200">
@@ -16,11 +17,21 @@ export class Card extends Component<CardProps> {
           alt={name}
           className="min-w-36 rounded-full object-cover"
         />
-        <div>
+        <div className="flex flex-col gap-2">
           <h3 className="text-2xl font-caveat">{name}</h3>
-          <p className="italic text-sm text-gray-800 dark:text-gray-200">
+          <p className="italic text-sm text-gray-800 dark:text-gray-100">
             {species} - {status}, {gender}
           </p>
+          {origin.name !== 'unknown' ? (
+            <p className="italic text-sm text-gray-800 dark:text-gray-50">
+              Native dimension: {origin.name}
+            </p>
+          ) : (
+            <p className="italic text-sm text-gray-800 dark:text-gray-50">
+              Dimension unknown. Last seen:{' '}
+              {location?.name ?? 'somewhere in the multiverse'}
+            </p>
+          )}
         </div>
       </div>
     );
