c0612d4662c97dde78d5c37bfe6bbc1cde81b455
maiano <maiano@yandex.ru>
2025-07-14 03:07:18 +0200

    chore: install vitest and set up test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
@@ -11,5 +11,41 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    coverage: {
+      all: true,
+      exclude: [
+        'src/test/**',
+        '**/types/**',
+        '**/*.d.ts',
+        'src/**/index.ts',
+        'src/main.tsx',
+      ],
+      extension: ['.ts', '.tsx'],
+      include: ['src/**/*'],
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        statements: 80,
+        lines: 50,
+        functions: 50,
+        branches: 50,
+      },
+    },
+    typecheck: {
+      enabled: true,
+      include: ['**/*.test-d.ts'],
+    },
+    css: {
+      modules: { classNameStrategy: 'non-scoped' },
+    },
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+    environment: 'jsdom',
+    globals: true,
+    maxConcurrency: 4,
+    setupFiles: ['./vitest.setup.mjs'],
+    cache: { dir: './node_modules/.vite/.vitest-cache' },
   },
 });
