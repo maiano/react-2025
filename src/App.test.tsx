3ccd9952999153020ee1302aeb95da79af341260
maiano <maiano@yandex.ru>
2025-07-17 18:58:08 +0200

    test: add test for the App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from './App';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { UI_STRINGS } from '@/shared/constants/ui-strings';
 
 describe('App component', () => {
   it('renders components', () => {
     render(<App />);
     expect(screen.getByText(UI_STRINGS.title)).toBeInTheDocument();
+  });
+
+  it('throw the error with the button', () => {
+    render(
+      <ErrorBoundary fallback={<div>App down</div>}>
+        <App />
+      </ErrorBoundary>,
+    );
+
+    const errorButton = screen.getByText(UI_STRINGS.errorButton);
+
+    fireEvent.click(errorButton);
+
+    expect(screen.getByText(/App down/i)).toBeInTheDocument();
   });
 });
