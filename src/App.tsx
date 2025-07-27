2311b71a3637b87369b10016652b50e6a3b79968
maiano <maiano@yandex.ru>
2025-07-27 22:57:27 +0200

    refactor: move error throwing logic to Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,14 @@ import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { HomePage } from '@/pages/HomePage';
 
-type AppState = {
-  wouldThrow: boolean;
-};
-
 class App extends Component {
-  state: AppState = {
-    wouldThrow: false,
-  };
-
   render(): ReactNode {
-    const { wouldThrow } = this.state;
-
-    if (wouldThrow) {
-      throw new Error('test error from button');
-    }
-
     return (
       <div className="flex min-h-screen bg-white">
         <div className="w-full max-w-7xl flex flex-col bg-gray-100 mx-auto">
           <Header />
           <HomePage />
-          <Footer onThrowError={() => this.setState({ wouldThrow: true })} />
+          <Footer />
         </div>
       </div>
     );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Button } from '@/components/Button';
 import { UI_STRINGS } from '@/shared/constants/ui-strings';
 
-type FooterProps = {
-  onThrowError: () => void;
-};
+export const Footer = () => {
+  const [wouldThrow, setWouldThrow] = useState(false);
 
-export const Footer = ({ onThrowError }: FooterProps) => {
+  if (wouldThrow) {
+    throw new Error('test error from button');
+  }
+
   return (
     <footer className="px-4 pb-4 flex justify-end">
-      <Button className="text-red-500 cursor-pointer" onClick={onThrowError}>
+      <Button
+        className="text-red-500 cursor-pointer"
+        onClick={() => setWouldThrow(true)}
+      >
         {UI_STRINGS.errorButton}
       </Button>
     </footer>
