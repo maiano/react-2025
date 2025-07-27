ccbca58c0371e2f0c6cc099775f8a24c873c8fc1
maiano <maiano@yandex.ru>
2025-07-28 00:18:24 +0200

    feat: add React Router with basic route configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { Component, type ReactNode } from 'react';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { ROUTES } from '@/app/routes';
+import { FallBack } from '@/components/FallBack';
+import { MainLayout } from '@/layouts/MainLayout';
+import { AboutPage } from '@/pages/AboutPage';
 import { HomePage } from '@/pages/HomePage';
+import { NotFoundPage } from '@/pages/NotFoundPage';
 
-class App extends Component {
-  render(): ReactNode {
-    return (
-      <div className="flex min-h-screen bg-white">
-        <div className="w-full max-w-7xl flex flex-col bg-gray-100 mx-auto">
-          <Header />
-          <HomePage />
-          <Footer />
-        </div>
-      </div>
-    );
-  }
+const router = createBrowserRouter([
+  {
+    path: ROUTES.HOME,
+    element: <MainLayout />,
+    errorElement: <FallBack />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: ROUTES.ABOUT, element: <AboutPage /> },
+      { path: ROUTES.NOT_FOUND, element: <NotFoundPage /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
