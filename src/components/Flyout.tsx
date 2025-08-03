0d625d8238c58b899101be341f467d6e761bf2d4
maiano <maiano@yandex.ru>
2025-08-03 22:01:43 +0200

    feat: add logic for saving characters to a file

--- src/components/Flyout.tsx
+++ src/components/Flyout.tsx
@@ -1,29 +1,31 @@
 import { Button } from '@/components/Button';
 import { UI_STRINGS } from '@/shared/constants/ui-strings';
+import { downloadCharactersCSV } from '@/shared/utils/download-characters-csv';
 import { useSelectedCharactersStore } from '@/store/selectedCharactersStore';
 
 export const Flyout = () => {
+  const getSelectedArray = useSelectedCharactersStore(
+    (state) => state.getSelectedArray,
+  );
   const selectedCount = useSelectedCharactersStore(
     (state) => state.selectedMap.size,
   );
-
   const unselectAll = useSelectedCharactersStore((state) => state.unselectAll);
 
   if (selectedCount === 0) return null;
 
+  const handleDownload = () => {
+    downloadCharactersCSV(getSelectedArray());
+  };
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-gray-600 p-4 flex justify-between items-center z-50">
-      <span className="text-base">
+      <span className="text-base" aria-live="polite">
         {UI_STRINGS.flyout.itemsSelected(selectedCount)}
       </span>
       <div className="flex gap-4">
         <Button onClick={unselectAll}>{UI_STRINGS.flyout.unselectAll}</Button>
-        <Button
-          onClick={() => {}}
-          // className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1 rounded"
-        >
-          {UI_STRINGS.flyout.download}
-        </Button>
+        <Button onClick={handleDownload}>{UI_STRINGS.flyout.download}</Button>
       </div>
     </div>
   );
