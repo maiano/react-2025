23982424f161dd61a8aee647f5ee5c6f2095b9d0
maiano <maiano@yandex.ru>
2025-08-24 23:42:06 +0200

    feat: change App.tsx to display the ControlledForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
 import { ControlledForm } from '@/components/ControlledForm';
 import { Modal } from '@/components/Modal';
+import { useFormStore } from '@/shared/store/useFormStore';
+import { Button } from '@/shared/ui/Button';
+import { Tile } from '@/shared/ui/Tile';
 
 function App() {
+  const [uncontrolledModal, setUncontrolledModal] = useState(false);
+  const [controlledModal, setControlledModal] = useState(false);
+
+  const uncontrolledData = useFormStore((state) => state.uncontrolledForms);
+  const controlledData = useFormStore((state) => state.controlledForms);
+
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <div className="flex flex-col items-center justify-center p-8 gap-8">
       <h1 className="text-xl">React Forms</h1>
-      <Modal isOpen={true} onClose={function (): void {}}>
-        <p>Modal window</p>
-        <ControlledForm onClose={function (): void {}} />
+
+      <div className="space-x-4">
+        <Button onClick={() => setUncontrolledModal(true)}>
+          Open Uncontrolled Form
+        </Button>
+        <Button onClick={() => setControlledModal(true)}>
+          Open Controlled Form
+        </Button>
+      </div>
+
+      {/* {uncontrolledData && <Tile data={uncontrolledData} isNew={true} />} */}
+      {controlledData && <Tile data={controlledData} isNew={true} />}
+
+      {/* <Modal isOpen={uncontrolledModal} onClose={() => setUncontrolledModal(false)}>
+        <UncontrolledForm onClose={() => setUncontrolledModal(false)} />
+      </Modal> */}
+
+      <Modal isOpen={controlledModal} onClose={() => setControlledModal(false)}>
+        <ControlledForm onClose={() => setControlledModal(false)} />
       </Modal>
     </div>
   );
